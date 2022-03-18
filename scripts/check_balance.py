137eaa6381326e1c9534b083182d4bf194af1910
Andrea Borghi <andrea.borghi@camptocamp.com>
2022-03-18 19:22:47 +0100

    cleanup check_balance

--- scripts/check_balance.py
+++ scripts/check_balance.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from ast import arg
 from api import (
     GEOSERVER_URL,
     GEOSERVER_REST_URL,
@@ -8,6 +9,8 @@ from api import (
 )
 
 from api.GeoserverApi import GeoserverAPI
+
+import argparse as ap
 from pprint import pprint
 import pandas
 from collections import Counter
@@ -40,31 +43,68 @@ class Bbox:
         return (self.ll.x, self.ll.y, self.ur.x, self.ur.y)
 
 
-fullConfig = {}
-instances = []
-
-geoserverServer = GeoserverAPI(
-    GEOSERVER_REST_URL, GEOSERVER_USERNAME, GEOSERVER_PASSWORD
-)
-
-headers = []
-
-for workspace_name in geoserverServer.list_workspaces():
-    fullConfig[workspace_name] = WebMapService(
-        GEOSERVER_URL + "/" + workspace_name + "/wms",
-        version="1.3.0",
-        username=GEOSERVER_USERNAME,
-        password=GEOSERVER_PASSWORD,
-    )
-
-# FIXME: should randomly select from the different workspaces as well
-wms = fullConfig[list(fullConfig.keys())[0]]
-print(type(wms))
-
+# FIXME: dirty global
 EPSG_3857_BBOX = Bbox(Point(-20037508, -20037508), Point(20037508, 20037508))
 
 
-def dirty_parallel_requests():
+def createParser():
+    parser = ap.ArgumentParser(
+        description="""
+This software runs several requests against a geoserver-cloud instance,
+and performs basic statistic with regard to the load-balancing.
+ 
+""",
+        epilog="""
+Disclaimer:
+    This software is provided \"as is\" and
+    is not granted to work in particular cases or without bugs. The author
+    disclaims any responsability in case of data loss, computer damage or any
+    other bad issue that could arise using this software.
+    Author:
+    Andrea Borghi, Camptocamp SA, Switzerland
+    last update: March, 2022
+Example:
+   check_balance -n 1000 -p 12 -o output_folder/output_file.png
+    """,
+        formatter_class=ap.RawDescriptionHelpFormatter,
+    )
+
+    optionGroup = parser.add_argument_group("Program options")
+    optionGroup.add_argument(
+        "-v", "--version", action="version", version="%(prog)s v0.1 Licence: WTFPL v2"
+    )
+
+    optionGroup.add_argument(
+        "-o",
+        "--output",
+        dest="output_file",
+        action="store",
+        default="/tmp/barplot.png",
+        type=str,
+        help="output file name to be used, the defalut is '/tmp/barplot.png'",
+    )
+    optionGroup.add_argument(
+        "-n",
+        "--number",
+        dest="nb_request",
+        action="store",
+        type=int,
+        default=100,
+        help="number of total requests that have to be performed, default is 100",
+    )
+    optionGroup.add_argument(
+        "-p",
+        "--processes",
+        dest="nb_process",
+        action="store",
+        type=int,
+        default=mp.cpu_count(),
+        help="number of parallel processes to start, default is 'mp.cpu_count()'",
+    )
+    return parser
+
+
+def parallel_requests(wms, dummy):
     try:
         img = wms.getmap(
             layers=[
@@ -90,24 +130,49 @@ def dirty_parallel_requests():
         return None
 
 
-# nb_parallel_requests = mp.cpu_count()
-nb_parallel_requests = 8
-nb_total_requests = 1000
+def main():
+    parser = createParser()
+    options = parser.parse_args()
 
-pool = mp.Pool(nb_parallel_requests)
-instances = list(
-    filter(
-        None, [pool.apply(dirty_parallel_requests) for i in range(nb_total_requests)]
+    fullConfig = {}
+
+    geoserverServer = GeoserverAPI(
+        GEOSERVER_REST_URL, GEOSERVER_USERNAME, GEOSERVER_PASSWORD
     )
-)
 
-pool.close()
+    for workspace_name in geoserverServer.list_workspaces():
+        fullConfig[workspace_name] = WebMapService(
+            GEOSERVER_URL + "/" + workspace_name + "/wms",
+            version="1.3.0",
+            username=GEOSERVER_USERNAME,
+            password=GEOSERVER_PASSWORD,
+        )
 
-instances_count = Counter(instances)
-df = pandas.DataFrame.from_dict(instances_count, orient="index")
-fig = df.plot(kind="bar").get_figure()
-fig.tight_layout(rect=(0, 0.1, 1, 1))
-fig.savefig("/tmp/barplot.png")
+    # FIXME: should randomly select from the different workspaces as well
+    wms = fullConfig[list(fullConfig.keys())[0]]
+    print(type(wms))
+
+    pool = mp.Pool(options.nb_process)
+    instances = list(
+        filter(
+            None,
+            [
+                pool.apply(parallel_requests, args=(wms, True))
+                for i in range(options.nb_request)
+            ],
+        )
+    )
+    pool.close()
+
+    instances_count = Counter(instances)
+    df = pandas.DataFrame.from_dict(instances_count, orient="index")
+    fig = df.plot(kind="bar").get_figure()
+    fig.tight_layout(rect=(0, 0.1, 1, 1))
+    fig.savefig(options.output_file)
+
+    pprint(instances)
+    pprint(len(instances))
 
 
-pprint(instances)
+if __name__ == "__main__":
+    main()
